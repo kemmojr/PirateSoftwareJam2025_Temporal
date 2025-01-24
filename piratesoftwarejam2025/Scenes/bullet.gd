172da31a5457db20662df6283a42e719322e6d4c
pkemmo4 <pkemmo4@users.noreply.github.com>
2025-01-24 16:29:36 +1100

    small change that was missed in last commit

--- piratesoftwarejam2025/Scenes/bullet.gd
+++ piratesoftwarejam2025/Scenes/bullet.gd
@@ -4,6 +4,5 @@ var bulletVelocity = Vector2(0, 1)
 var bulletSpeed = 1000
 
 func _physics_process(delta):
-		#velocity = bulletVelocity.normalized() * delta * bulletSpeed
-		#move_and_slide()
-		pass
+		velocity = bulletVelocity.normalized() * delta * bulletSpeed
+		move_and_slide()
