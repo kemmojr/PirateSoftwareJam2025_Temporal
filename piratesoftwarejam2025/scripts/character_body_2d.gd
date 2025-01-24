4f068e3662cfca33ab6ffc741a34a0949343de7c
pkemmo4 <pkemmo4@users.noreply.github.com>
2025-01-24 15:02:05 +1100

    Turret Prototype, currently only looks at the player

--- piratesoftwarejam2025/scripts/character_body_2d.gd
+++ piratesoftwarejam2025/scripts/character_body_2d.gd
@@ -1,41 +1,25 @@
 extends CharacterBody2D
 
-@onready var animated_sprite = $AnimatedSprite2D
 
-var alive = true
-const SPEED = 200
-const JUMP_VELOCITY = -200.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add gravity.
+	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
-	if alive == true:
-		var direction := Input.get_axis("move_left", "move_right")
-		move(direction)
-		flip(direction)
-	elif alive == false:
-		velocity.x = 0
-		animated_sprite.modulate = Color(1,0.33,0.33,1)
-		
-	
-	move_and_slide()
 
-func player():
-	pass #function check whether a body is the player
-func move(direction):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-func flip(direction):
-	if direction < 0:
-		animated_sprite.flip_h = true
-	elif direction > 0:
-		animated_sprite.flip_h = false
-		
+
+	move_and_slide()
