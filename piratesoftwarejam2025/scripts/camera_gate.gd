98020051502a9ee666a2126dbfba413489f5a6a4
pkemmo4 <pkemmo4@users.noreply.github.com>
2025-01-25 13:49:32 +1100

    camera gate animation working

--- piratesoftwarejam2025/scripts/camera_gate.gd
+++ piratesoftwarejam2025/scripts/camera_gate.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
 @onready var static_body_2d = $StaticBody2D
+@onready var gate = $Gate
 
 func _ready(): 
 	await get_tree().create_timer(1).timeout # wait a second for the other assets to load in
 	static_body_2d.position.y = -33
-
+	gate.speed_scale = 1
+	gate.play("default")
+	
 func _on_area_2d_body_entered(_body): # used _body because body is not used within the method
 	static_body_2d.position.y = 0
+	gate.speed_scale = -1 # play the animation in reverse to close the gate
+	gate.play("default")
 	
 func _on_area_2d_body_exited(_body):
 	static_body_2d.position.y = -33
+	gate.speed_scale = 1 
+	gate.play("default")
